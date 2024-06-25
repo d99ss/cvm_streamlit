dc07a39f67e7bdd0af9660c7544d11d9a3a438a0
David <mrbettencourt99@gmail.com>
2024-06-25 16:53:35 -0300

    Update main.py
    Added ADS top,bottom and navbar of the page

--- main.py
+++ main.py
@@ -8,19 +8,35 @@ import info_diario
 import about
 import streamlit.components.v1 as components
 
-
-# Load environment variables from .env filemyenv\Scripts\activate
-
+# Load environment variables from .env file
 load_dotenv()
 
 # Set the page title for the Streamlit app
 st.set_page_config(page_title="Consulta CVM")
 
-
+# Your custom HTML with the script tag for monetization
+custom_html = """
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- Add any additional head content here -->
+</head>
+<body>
+    <script type="text/javascript">
+        atOptions = {
+            'key' : '6061c3328cd4a2114ac481b4cbae2cde',
+            'format' : 'iframe',
+            'height' : 60,
+            'width' : 468,
+            'params' : {}
+        };
+    </script>
+    <script type="text/javascript" src="//www.topcreativeformat.com/6061c3328cd4a2114ac481b4cbae2cde/invoke.js"></script>
+</body>
+</html>
+"""
 
 # Function to run the Streamlit app with multi-page navigation
-
-
 class MultiApp:
     def __init__(self):
         self.apps = []
@@ -34,12 +50,11 @@ class MultiApp:
     def run(self):
         # Sidebar navigation using option_menu
         with st.sidebar:
+            components.html(custom_html, height=100)  # Embed the custom HTML in the sidebar
             app = option_menu(
                 menu_title='Main Menu',
-                options=['Home', 'Cadastro de Fundos',
-                         'Fundos: Informação diária', 'About'],
-                icons=['house', 'person-circle',
-                       'clipboard-pulse', 'info-circle'],
+                options=['Home', 'Cadastro de Fundos', 'Fundos: Informação diária', 'About'],
+                icons=['house', 'person-circle', 'clipboard-pulse', 'info-circle'],
                 menu_icon='cast',
                 default_index=0,  # Default to 'Home' page
                 styles={
@@ -59,33 +74,12 @@ class MultiApp:
             info_cadastral.app()
         elif app == 'About':
             about.app()
-            
 
-# Your custom HTML with the script tag for monetization
-custom_html = """
-<!DOCTYPE html>
-<html>
-<head>
-    <!-- Add any additional head content here -->
-</head>
-<body>
-    <script type="text/javascript">
-	atOptions = {
-		'key' : '6061c3328cd4a2114ac481b4cbae2cde',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//www.topcreativeformat.com/6061c3328cd4a2114ac481b4cbae2cde/invoke.js"></script>
-</body>
-</html>
-"""
+        # Embed the custom HTML at the bottom of the page
+        components.html(custom_html, height=100)
 
-# Embed the custom HTML in your Streamlit app
-components.html(custom_html, height=500)        
-
+# Embed the custom HTML at the top of the page
+components.html(custom_html, height=100)
 
 # Instantiate MultiApp and run the application
 if __name__ == "__main__":
