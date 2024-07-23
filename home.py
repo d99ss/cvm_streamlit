c32039134197ab9d11895560f48d69da3813e9d9
David <mrbettencourt99@gmail.com>
2024-07-23 15:32:00 -0300

    Update home.py

--- home.py
+++ home.py
@@ -4,8 +4,9 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 
 def app():
-    st.title("Home Page")
-    st.write("A consulta aos Fundos de Investimento registrados na CVM pode ser feita por CNPJ ou por parte do nome do fundo.")
-    st.write("Podem ser obtidas todas as informações públicas dos fundos, tais como o valor diário da cota e do patrimônio líquido, o número de cotistas, valores captados e resgatados. Ainda é possível consultar o Regulamento, o Prospecto, a Lâmina de Informações essenciais, a Composição da carteira, os Fatos Relevantes e os Balancetes de cada Fundo.")
-    st.write("Consulte, através dos ícones abaixo, os Fundos de Investimento registrados na CVM e os cancelados (disponíveis na Central de Sistemas.")
-    # Add more Streamlit components as needed
+    st.header("Consulta CVM")
+    st.write("O consultacvm.streamlit.app disponibiliza a consulta aos Fundos de Investimento registrados na CVM de forma gratuita.")
+    st.write("Podem ser obtidas todas as informações públicas dos fundos, tais como dados cadastrais, valor diário da cota e do patrimônio líquido, número de cotistas, valores captados e resgatados.")
+    st.write("Consulte, através dos ícones, os dados cadastrais dos fundos de investimento registrados na CVM e o INFORME DIÁRIO.")
+    st.write(f"Todos os dados são obtidos de forma gratuita através do **Portal Dados Abertos CVM** e atualizados a cada atualização da página.")
+
